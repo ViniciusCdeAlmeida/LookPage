01aabe426a4cee6a5619c713a49e59f644f1a658
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2020-02-21 10:24:40 -0800

    Heroku 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {Angular2TokenService} from "angular2-token";
 import {environment} from "../environments/environment";
-// import { Http, Response, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -12,26 +11,5 @@ export class AppComponent {
   title = 'LookPageFront';
   constructor(private authToken: Angular2TokenService){
     this.authToken.init(environment.token_auth_config);
-    // this.authToken.signIn({email: "look@teracode.com", password: "lookafter"}).subscribe(
-
-    //   res => {
-
-    //     console.log('auth response:', res);
-    //     console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
-    //     console.log('auth response body:', res.json()); //log the response body to show the user 
-    //   },
-
-    //   err => {
-    //     console.error('auth error:', err);
-    //   }
-    // )
   }
 }
-
-
-// curl -v -X "POST" "http://localhost:3000/auth/sign_in" \
-//      -H "Content-Type: application/json; charset=utf-8" \
-//      -d $'{
-//   "email": "look@teracode.com",
-//   "password": "lookafter"
-// }'
